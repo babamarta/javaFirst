3621c881a8cc9f1632ddc750637c9afd752a3118
YuliaGorshkova <yu.gorshkova@gmail.com>
2018-10-31 20:26:29 +0300

    Градл заработал, наконец!

--- src/main/java/ru/yu_go/MyFirstProgram.java
+++ src/main/java/ru/yu_go/MyFirstProgram.java
@@ -3,5 +3,8 @@ package ru.yu_go;
 public class MyFirstProgram {
     public static void main(String[] args) {
         System.out.println("Привет, мир!");
+
+        System.out.println((2+2)+"34");
+
     }
 }
