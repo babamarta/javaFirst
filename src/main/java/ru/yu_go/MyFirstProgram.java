094e010f6d61b42d5b7c66e1bb1c894cb612bce6
YuliaGorshkova <yu.gorshkova@gmail.com>
2018-11-01 09:33:00 +0300

    Про математику слегка

--- src/main/java/ru/yu_go/MyFirstProgram.java
+++ src/main/java/ru/yu_go/MyFirstProgram.java
@@ -2,11 +2,24 @@ package ru.yu_go;
 
 public class MyFirstProgram {
     public static void main(String[] args) {
-        System.out.println("Привет, мир!");
+        hello ("Кипр") ;
+        double l = 5;
+        double w = 4;
+        double v = 17;
+        System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
+        System.out.println("Площадь прямоугольника со сторонами " + w + " и " + v + " = " + area(w,v));
+}
 
-        int l = 6;
-        int s = l*l;
-        System.out.println("Площадь квадрата со стороной " + l + " = " + s);
+    public static void hello (String somebody ) {
 
+        System.out.println("Переезжаю на "  + somebody + "!");
+    }
+
+    public static double area (double a){
+        return a*a;
+    }
+
+    public static double area (double a, double b){
+        return a*b;
     }
 }
